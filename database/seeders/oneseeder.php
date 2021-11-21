8d31201028dca3bd7cdb1f25e4476afdcf1250f0
godgodwinter <kakadz.pro@gmail.com>
2021-11-21 22:22:51 +0700

    update seeder baru

--- database/seeders/oneseeder.php
+++ database/seeders/oneseeder.php
@@ -76,5 +76,62 @@ class oneseeder extends Seeder
           ]);
 
 
+          $faker = Faker::create('id_ID');
+
+          $nama='Paimin';
+          $nomerinduk='123';
+          $users_id=DB::table('users')->insertGetId([
+              'name' =>  $nama,
+              'email' => $faker->unique()->email,
+              'username'=>'pelatih',
+              'nomerinduk'=>$nomerinduk,
+              'password' => Hash::make('pelatih'),
+              'tipeuser' => 'pelatih',
+              'created_at' => Carbon::now(),
+              'updated_at' => Carbon::now()
+          ]);
+
+
+          DB::table('pelatih')->insert([
+              'nama' => $nama,
+              'jk' => $faker->randomElement(['Laki-laki', 'Perempuan']),
+              'alamat' => $faker->unique()->address,
+              'telp' => $faker->unique()->phoneNumber,
+              'tgllahir' => $faker->numberBetween(1990,2020).'-0'.$faker->numberBetween(1,9).'-'.$faker->numberBetween(10,29),
+              'spesialis' => $faker->randomElement(['Taktik', 'Teknik','Fisik']),
+              'users_id' => $users_id,
+              'created_at' => Carbon::now(),
+              'updated_at' => Carbon::now()
+          ]);
+
+
+
+          $nama='Paijo';
+          $nomerinduk='111';
+          $users_id=DB::table('users')->insertGetId([
+              'name' =>  $nama,
+              'email' => $faker->unique()->email,
+              'username'=>'pemain',
+              'nomerinduk'=>$nomerinduk,
+              'password' => Hash::make('pemain'),
+              'tipeuser' => 'pemain',
+              'created_at' => Carbon::now(),
+              'updated_at' => Carbon::now()
+          ]);
+
+
+          DB::table('pemain')->insert([
+              'nama' => $nama,
+              'jk' => $faker->randomElement(['Laki-laki', 'Perempuan']),
+              'alamat' => $faker->unique()->address,
+              'telp' => $faker->unique()->phoneNumber,
+              'tgllahir' => $faker->numberBetween(1990,2020).'-0'.$faker->numberBetween(1,9).'-'.$faker->numberBetween(10,29),
+              'tgldaftar' => $faker->numberBetween(1990,2020).'-0'.$faker->numberBetween(1,9).'-'.$faker->numberBetween(10,29),
+              'users_id' => $users_id,
+              'created_at' => Carbon::now(),
+              'updated_at' => Carbon::now()
+          ]);
+
+
     }
 }
