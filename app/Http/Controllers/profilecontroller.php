9ef35ca035f9545daabc697beed1db781ecd79c4
godgodwinter <kakadz.pro@gmail.com>
2021-12-06 16:43:18 +0700

    add menu update profile auth pemain

--- app/Http/Controllers/profilecontroller.php
+++ app/Http/Controllers/profilecontroller.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\pelatih;
+use App\Models\pemain;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -117,5 +118,104 @@ class profilecontroller extends Controller
 
 
     return redirect()->route('pelatih.profile')->with('status','Data berhasil diubah!')->with('tipe','success')->with('icon','fas fa-feather');
+    }
+    public function pemain(){
+
+        $pages='profile';
+
+
+
+        $datas=DB::table('users')->where('id',Auth::user()->id)->first();
+        $data=pemain::with('users')->where('users_id',Auth::user()->id)->first();
+        return view('pages.admin.settings.pemain',compact('datas','pages','data'));
+    }
+    public function pemainupdate (Request $request)
+    {
+        $myprofile=Auth::user();
+        // dd($myprofile,$request->username);
+
+        if($myprofile->username!==$request->username){
+
+            $request->validate([
+                'username' => "required|unique:users,username",
+            ],
+            [
+            ]);
+        }
+
+        $request->validate([
+            'name'=>'required',
+            'email'=>'required',
+            'username'=>'required',
+        ],
+        [
+            'nama.required'=>'nama harus diisi',
+        ]);
+
+
+        if($request->password!=null OR $request->password!=''){
+
+        $request->validate([
+            'password' => 'min:3|required_with:password2|same:password2',
+            'password2' => 'min:3',
+        ],
+        [
+            'nama.required'=>'nama harus diisi',
+        ]);
+            User::where('id',$myprofile->id)
+            ->update([
+                'name'     =>   $request->name,
+                'email'     =>   $request->email,
+                'username'     =>   $request->username,
+                'password' => Hash::make($request->password),
+               'updated_at'=>date("Y-m-d H:i:s")
+            ]);
+        }else{
+            User::where('id',$myprofile->id)
+            ->update([
+                'name'     =>   $request->name,
+                'username'     =>   $request->username,
+                'email'     =>   $request->email,
+               'updated_at'=>date("Y-m-d H:i:s")
+            ]);
+
+        }
+
+        $data=pemain::with('users')->where('users_id',Auth::user()->id)->first();
+
+        pemain::where('id',$data->id)
+        ->update([
+            'nama'     =>   $request->name,
+            'telp'     =>   $request->telp,
+            'tgllahir'     =>   $request->tgllahir,
+            'alamat'     =>   $request->alamat,
+            'jk'     =>   $request->jk,
+           'updated_at'=>date("Y-m-d H:i:s")
+        ]);
+
+        $files = $request->file('files');
+
+
+        if($files!=null){
+
+
+            // dd('storage'.'/'.$id->sekolah_logo);
+            $namafilebaru=$data->id;
+            $tujuan_upload = 'storage/pemain';
+                    // upload file
+            $files->move($tujuan_upload,"pemain/".$namafilebaru.".jpg");
+
+            $photo="pemain/".$namafilebaru.".jpg";
+
+
+            pemain::where('id',$data->id)
+            ->update([
+                'photo'     =>   $photo,
+            'updated_at'=>date("Y-m-d H:i:s")
+            ]);
+        }
+
+
+    return redirect()->route('pemain.profile')->with('status','Data berhasil diubah!')->with('tipe','success')->with('icon','fas fa-feather');
     }
 }
--- resources/views/pages/admin/settings/pemain.blade.php
+++ resources/views/pages/admin/settings/pemain.blade.php
@@ -58,6 +58,54 @@
                             @enderror
                         </div>
 
+
+                        <div class="form-group col-md-5 col-5 mt-0 ml-5">
+                            <label for="alamat">Alamat<code>*)</code></label>
+                            <input type="text" name="alamat" id="alamat" class="form-control @error('alamat') is-invalid @enderror" value="{{old('alamat')?old('alamat'):$data->alamat}}" required>
+                            @error('alamat')<div class="invalid-feedback"> {{$message}}</div>
+                            @enderror
+                        </div>
+
+
+                        <div class="form-group col-md-5 col-5 mt-0 ml-5">
+                            <label for="tgllahir">Tanggal Lahir<code>*)</code></label>
+                            @php
+                                if($data->tgllahir)
+                                    $tgl=$data->tgllahir;
+                                else
+                                    $tgl=date('Y-m-d');
+                            @endphp
+                            <input type="date" name="tgllahir" id="tgllahir" class="form-control @error('tgllahir') is-invalid @enderror" value="{{old('tgllahir')?old('tgllahir') : $tgl}}" required>
+                            @error('tgllahir')<div class="invalid-feedback"> {{$message}}</div>
+                            @enderror
+                        </div>
+
+                        <div class="form-group col-md-5 col-5 mt-0 ml-5">
+                            <label for="jk">Pilih Jenis Kelamin <code></code></label>
+
+                            <select class="form-control  @error('jk') is-invalid @enderror" name="jk" required>
+                                @if(old('jk'))
+                                <option>{{old('jk')}}</option>
+                            @else
+                                @if($data->jk)
+                                <option>{{$data->jk}}</option>
+                                @endif
+                            @endif
+                                <option>Laki-laki</option>
+                                <option>Perempuan</option>
+                            </select>
+                            @error('jk')<div class="invalid-feedback"> {{$message}}</div>
+                            @enderror
+                        </div>
+
+                        <div class="form-group col-md-5 col-5 mt-0 ml-5">
+                            <label for="telp">No WA <code>*)</code></label>
+                            <input type="text" class="form-control  @error('telp') is-invalid @enderror" name="telp" required value="{{$data->telp}}">
+
+                            @error('telp')<div class="invalid-feedback"> {{$message}}</div>
+                            @enderror
+                        </div>
+
                         <div class="form-group col-md-5 col-5 mt-0 ml-5">
                             <label for="password">Password <code>*)</code></label>
                             <input type="password" name="password" id="password" class="form-control @error('password') is-invalid @enderror" >
@@ -73,6 +121,51 @@
                         </div>
 
 
+                        @push('after-script')
+                        <script type="text/javascript">
+                            $(document).ready(function() {
+                              $.uploadPreview({
+                                input_field: "#image-upload",   // Default: .image-upload
+                                preview_box: "#image-preview",  // Default: .image-preview
+                                label_field: "#image-label",    // Default: .image-label
+                                label_default: "Logo Sekolah",   // Default: Choose File
+                                label_selected: "Ganti Logo Sekolah",  // Default: Change File
+                                no_label: false                 // Default: false
+                              });
+
+
+
+                            });
+                            </script>
+                        @endpush
+
+                        </div>
+                        <div class="row">
+                        <div class="form-group col-md-5 col-5 mt-0 ml-5">
+                          <div id="image-preview" class="image-preview">
+                            <label for="image-upload" id="image-label2">UPLOAD FOTO</label>
+                            <input type="file" name="files" id="image-upload" class="@error('files')
+                            is_invalid
+                        @enderror"  accept="image/png, image/gif, image/jpeg" />
+
+                        @error('files')<div class="invalid-feedback"> {{$message}}</div>
+                        @enderror
+                          </div>
+                        </div>
+
+
+                        <div class="form-group col-md-5 col-5 mt-0 ml-5">
+                            @php
+                            $gambar=$data->photo;
+                            $randomimg='https://ui-avatars.com/api/?name='.$data->nama.'&color=7F9CF5&background=EBF4FF';
+                            @endphp
+                            @if($data->photo!=null AND $data->photo!=url('storage') AND $data->photo!='')
+                            <img alt="image" src="{{$gambar}}" class="img-thumbnail" data-toggle="tooltip" title="{{$data->nama}}" width="200px" height="200px" style="object-fit:cover;">
+                            @else
+                            <img alt="image" src="{{$randomimg}}" class="img-thumbnail" data-toggle="tooltip" title="{{$data->nama}}" width="200px" height="200px" style="object-fit:cover;">
+
+                            @endif
+                          </div>
 
 
                         </div>
